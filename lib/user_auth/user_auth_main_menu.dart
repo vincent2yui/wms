5875355f04fd8327f0f096c9da2d08fa57f38ed2
vincent2yui <vincent2yui@yahoo.com>
2021-11-22 15:36:30 +0300

    Add dynamic menu based from user profile

--- lib/user_auth/user_auth_main_menu.dart
+++ lib/user_auth/user_auth_main_menu.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:states_rebuilder/states_rebuilder.dart';
 import 'package:wms/core/color_style.dart';
+import 'package:wms/core/entity/user.dart';
 import 'package:wms/main.dart';
 
 import 'widgets/user_auth_activity_card.dart';
 
 class MainMenu extends StatelessWidget {
   const MainMenu({Key? key}) : super(key: key);
+
+  get loginUser => authService.state.loginUser(User(
+        username: username.state.username.toUpperCase(),
+        password: password.state.password,
+      ));
 
   @override
   Widget build(BuildContext context) {
@@ -22,46 +28,52 @@ class MainMenu extends StatelessWidget {
           )
         ],
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(18),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          mainAxisExtent: 118,
-        ),
-        children: const [
-          ActivityCard(
-            activityName: "Receiving",
-            activityNameLogo: "receiving1.png",
-            isActive: true,
-          ),
-          ActivityCard(
-            activityName: "Put Away",
-            activityNameLogo: "put_away1.png",
-            isActive: false,
-          ),
-          ActivityCard(
-            activityName: "Picking",
-            activityNameLogo: "picking1.png",
-            isActive: false,
-          ),
-          ActivityCard(
-            activityName: "Loading",
-            activityNameLogo: "loading1.png",
-            isActive: true,
-          ),
-          ActivityCard(
-            activityName: "Replenishment",
-            activityNameLogo: "replenishment1.png",
-            isActive: true,
-          ),
-          ActivityCard(
-            activityName: "Cycle Count",
-            activityNameLogo: "cycle_count1.png",
-            isActive: true,
-          ),
-        ],
+      body: OnReactive(
+        () => FutureBuilder<User>(
+            future: loginUser,
+            builder: (context, snapshot) {
+              return GridView(
+                padding: const EdgeInsets.all(18),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  mainAxisExtent: 118,
+                ),
+                children: [
+                  ActivityCard(
+                    activityName: "Receiving",
+                    activityNameLogo: "receiving1.png",
+                    isActive: snapshot.data!.isReceivingActive == 'X',
+                  ),
+                  ActivityCard(
+                    activityName: "Put Away",
+                    activityNameLogo: "put_away1.png",
+                    isActive: snapshot.data!.isPutAwayActive == 'X',
+                  ),
+                  ActivityCard(
+                    activityName: "Picking",
+                    activityNameLogo: "picking1.png",
+                    isActive: snapshot.data!.isPickingActive == 'X',
+                  ),
+                  ActivityCard(
+                    activityName: "Loading",
+                    activityNameLogo: "loading1.png",
+                    isActive: snapshot.data!.isLoadingActive == 'X',
+                  ),
+                  ActivityCard(
+                    activityName: "Replenishment",
+                    activityNameLogo: "replenishment1.png",
+                    isActive: snapshot.data!.isReplenishmentActive == 'X',
+                  ),
+                  ActivityCard(
+                    activityName: "Cycle Count",
+                    activityNameLogo: "cycle_count1.png",
+                    isActive: snapshot.data!.isCycleCountActive == 'X',
+                  ),
+                ],
+              );
+            }),
       ),
     );
   }
