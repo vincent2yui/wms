346f22505ae5da632717b114fd860c652bf663e2
vincent2yui <vincent2yui@yahoo.com>
2021-11-23 10:34:21 +0300

    Add logout feature in main menu for android back button and logout button in appbar

--- lib/user_auth/user_auth_main_menu.dart
+++ lib/user_auth/user_auth_main_menu.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:states_rebuilder/states_rebuilder.dart';
 import 'package:wms/core/entity/user.dart';
@@ -15,64 +16,93 @@ class MainMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: OnReactive(() => Text(username.state.username)),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout_rounded),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: OnReactive(
-        () => FutureBuilder<User>(
-            future: loginUser,
-            builder: (context, snapshot) {
-              return GridView(
-                padding: const EdgeInsets.all(18),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  mainAxisExtent: 118,
-                ),
-                children: [
-                  ActivityCard(
-                    activityName: "Receiving",
-                    activityNameLogo: "receiving1.png",
-                    isActive: snapshot.data?.isReceivingActive == 'X',
+    return WillPopScope(
+      onWillPop: () async {
+        return await _onLogoutBackPress(context);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: OnReactive(() => Text(username.state.username)),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.logout_rounded),
+              onPressed: () async {
+                return await _onLogoutBackPress(context);
+              },
+            )
+          ],
+        ),
+        body: OnReactive(
+          () => FutureBuilder<User>(
+              future: loginUser,
+              builder: (context, snapshot) {
+                return GridView(
+                  padding: const EdgeInsets.all(18),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    mainAxisExtent: 118,
                   ),
-                  ActivityCard(
-                    activityName: "Put Away",
-                    activityNameLogo: "put_away1.png",
-                    isActive: snapshot.data?.isPutAwayActive == 'X',
-                  ),
-                  ActivityCard(
-                    activityName: "Picking",
-                    activityNameLogo: "picking1.png",
-                    isActive: snapshot.data?.isPickingActive == 'X',
-                  ),
-                  ActivityCard(
-                    activityName: "Loading",
-                    activityNameLogo: "loading1.png",
-                    isActive: snapshot.data?.isLoadingActive == 'X',
-                  ),
-                  ActivityCard(
-                    activityName: "Replenishment",
-                    activityNameLogo: "replenishment1.png",
-                    isActive: snapshot.data?.isReplenishmentActive == 'X',
-                  ),
-                  ActivityCard(
-                    activityName: "Cycle Count",
-                    activityNameLogo: "cycle_count1.png",
-                    isActive: snapshot.data?.isCycleCountActive == 'X',
-                  ),
-                ],
-              );
-            }),
+                  children: [
+                    ActivityCard(
+                      activityName: "Receiving",
+                      activityNameLogo: "receiving1.png",
+                      isActive: snapshot.data?.isReceivingActive == 'X',
+                    ),
+                    ActivityCard(
+                      activityName: "Put Away",
+                      activityNameLogo: "put_away1.png",
+                      isActive: snapshot.data?.isPutAwayActive == 'X',
+                    ),
+                    ActivityCard(
+                      activityName: "Picking",
+                      activityNameLogo: "picking1.png",
+                      isActive: snapshot.data?.isPickingActive == 'X',
+                    ),
+                    ActivityCard(
+                      activityName: "Loading",
+                      activityNameLogo: "loading1.png",
+                      isActive: snapshot.data?.isLoadingActive == 'X',
+                    ),
+                    ActivityCard(
+                      activityName: "Replenishment",
+                      activityNameLogo: "replenishment1.png",
+                      isActive: snapshot.data?.isReplenishmentActive == 'X',
+                    ),
+                    ActivityCard(
+                      activityName: "Cycle Count",
+                      activityNameLogo: "cycle_count1.png",
+                      isActive: snapshot.data?.isCycleCountActive == 'X',
+                    ),
+                  ],
+                );
+              }),
+        ),
       ),
     );
+  }
+
+  Future<dynamic> _onLogoutBackPress(BuildContext context) async {
+    debugPrint('Back button press');
+    return (await showCupertinoDialog(
+          context: context,
+          builder: (context) => CupertinoAlertDialog(
+            title: const Text('Logout'),
+            content: const Text('Do you want to logout?'),
+            actions: <CupertinoDialogAction>[
+              CupertinoDialogAction(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: const Text('No'),
+              ),
+              CupertinoDialogAction(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: const Text('Yes'),
+              ),
+            ],
+          ),
+        )) ??
+        false;
   }
 }
